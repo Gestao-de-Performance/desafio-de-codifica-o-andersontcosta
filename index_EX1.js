48911444e2ef1204c443600bc2e1934bda906541
Anderson Trindade Costa <andersontcosta@gmail.com>
2024-11-12 19:10:14 -0300

    Versão 02

--- index_EX1.js
+++
@@ -1,6 +0,0 @@
-function soma() {
-    let a = 199.3;
-    let b = 120;
-    let resultado = a + b;
-    console.log("O resultado da soma é: " + resultado);
-}
--- index_EX2.js
+++
@@ -1,6 +0,0 @@
-function media() {
-    let numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-    let soma = numeros.reduce((a, b) => a + b, 0);
-    let media = soma / numeros.length;
-    console.log("O resultado da média é: " + media);
-}
